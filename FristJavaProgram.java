716eda7f859f57d109a86cdc83398bffa3b55e7d
naresh551 <103302416+naresh551@users.noreply.github.com>
2022-04-10 12:35:32 +0530

    Add files via upload

---
+++ FristJavaProgram.java
@@ -0,0 +1,24 @@
+package coreJava;
+
+public class FristJavaProgram 
+{	
+	public static void main(String[] args) 
+	{
+		System.out.println(" This is a sample Java Program");	
+		m1();
+		//FristJavaProgram.m1();
+	}		
+
+	
+	public static void m1()
+	{
+		System.out.println(" The m1() method is called and executed");
+		m2();
+	}
+	
+	public static void m2()
+	{
+		System.out.println(" The m2() method is called and executed");
+	}
+}
+
