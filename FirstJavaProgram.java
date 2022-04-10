110c5f8ed5a586ca465c6433a1504626e2b35fce
naresh551 <103302416+naresh551@users.noreply.github.com>
2022-04-10 12:54:32 +0530

    Add files via upload

---
+++ FirstJavaProgram.java
@@ -0,0 +1,20 @@
+package corejava;
+
+public class FirstJavaProgram 
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println(" This is first Java program");
+		sheela();
+
+	}
+	
+	public static void sheela()
+	{
+		System.out.println(" user defined method is called");
+	}
+
+}
+
+
